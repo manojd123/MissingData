f6b56a7afda38910c0ec9bbf3cb62646491fdab5
manojd123 <59951046+manojd123@users.noreply.github.com>
2020-08-13 23:36:55 +0530

    Add files via upload

---
+++ missingdata.py
@@ -0,0 +1,39 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types   import StructType,StructField,StringType,IntegerType
+from pyspark.sql import functions as psf
+
+spark  = SparkSession.builder.appName("missing data ").master("local[*]").getOrCreate()
+sc = spark.sparkContext
+
+# Defining Schema
+retail_schema = StructType([
+    StructField('Sales_rep',StringType(), True),
+    StructField('region',StringType(), True),
+    StructField('Month',StringType(), True),
+    StructField('Sales',IntegerType(), True)
+
+])
+
+
+df = spark.read\
+    .schema(retail_schema)\
+    .option("delimeter","|")\
+    .csv("/home/manoj/Downloads/datasets/retail_data.csv") \
+
+df.show(10, False)
+
+validData = df.filter(psf.col("Region").isNotNull())
+invalidData= df.filter(psf.col("Region").isNull() | psf.col("Month").isNull())\
+    .withColumn("Hold_reason", psf
+                .when(psf.col("Region").isNull(), "Region is missing")
+                .otherwise(psf.when(psf.col("Month").isNull(), "Month is missing")))
+
+validData.write\
+    .mode("overwrite")\
+    .option("delimeter",",")\
+    .csv("/home/manoj/Downloads/datasets/valid")
+
+invalidData.write\
+    .mode("append")\
+    .option("delimeter",",")\
+    .csv("/home/manoj/Downloads/datasets/invalid")
